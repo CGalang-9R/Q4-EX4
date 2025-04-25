d85a3ddb8981062c652b9674c497647014bb5c3d
CGalang-9R <galang.chm@obmontessori.edu.ph>
2025-04-25 08:52:33 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function libsSection() {
+	var library_section = document.getElementById('enterSection').value;
+
+	switch(library_section) {
+	case "F":
+	document.getElementById('sectionResult').innerHTML="You have chosen fiction!"; 
+	break;
+
+	case "N":
+	document.getElementById('sectionResult').innerHTML="You have chosen non-fiction!";
+	break;
+
+	case "R":
+	document.getElementById('sectionResult').innerHTML="You have chosen reference!";
+	break;
+
+	default:
+	document.getElementById('sectionResult').innerHTML="Invalid entry!";
+	}
+}
+
+function classSection() {
+	var class_section = document.getElementById('enterClass').value;
+
+	switch(class_section) {
+	case "1":
+	document.getElementById('sectionClass').innerHTML="Emerald"; 
+	break;
+
+	case "2":
+	document.getElementById('sectionClass').innerHTML="Ruby";
+	break;
+
+	case "3":
+	document.getElementById('sectionClass').innerHTML="Topaz";
+	break;
+
+	case "4":
+	document.getElementById('sectionClass').innerHTML="Sapphire";
+	break;
+
+	default:
+	document.getElementById('sectionClass').innerHTML="Invalid entry!";
+	}
+}
